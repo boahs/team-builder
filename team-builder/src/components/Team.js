7edfb99f214062064fbd0942c09c38188991f9bb
boahs <39522778+boahs@users.noreply.github.com>
2020-08-18 22:38:22 -0400

    refactor

--- team-builder/src/components/Team.js
+++ team-builder/src/components/Team.js
@@ -1,17 +1,27 @@
 import React from "react";
+import styled from "styled-components";
+
+const rand = "#" + Math.floor(Math.random() * 0xffffff).toString(16);
+console.log(rand);
+
+const StyledDiv = styled.div`
+  border: 3px solid ${rand};
+`;
 
 export default function TeamMembers(props) {
   const { details } = props;
 
   if (!details) {
-    return <h3>Working fetching your Team Members&apos;s details...</h3>;
+    return <h3>Working on obtaining your Team members&apos;s details...</h3>;
   }
 
   return (
     <div className="Team Members container">
-      <h2>{details.name}</h2>
-      <p>Email: {details.email}</p>
-      <p>Role: {details.role}</p>
+      <StyledDiv>
+        <p>Name: {details.name}</p>
+        <p>Email: {details.email}</p>
+        <p>Role: {details.role}</p>
+      </StyledDiv>
     </div>
   );
 }
