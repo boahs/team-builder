fabded0435ba4265c9d8f9d5ce1f6d98398fec28
boahs <39522778+boahs@users.noreply.github.com>
2020-08-19 03:51:27 -0400

    added styled component css animations

--- team-builder/src/components/Team.js
+++ team-builder/src/components/Team.js
@@ -1,8 +1,32 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
 // const rand = "#" + Math.floor(Math.random() * 0xffffff).toString(16);
 // console.log(rand); // couldn't get it to loop with new entires oh well
+
+// injectGlobal`
+// @font-face {
+//   font-family: "Open Sans Condensed";
+//   src: url('https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap')
+// }
+// `;
+
+const rotate = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+
+  to {
+    transform: rotate(360deg);
+  }
+`;
+const Rotate = styled.div`
+  display: inline-block;
+  animation: ${rotate} 4s linear infinite;
+  padding: 2rem 1rem;
+  font-size: 1.2rem;
+  font-size: 1.2rem;
+`;
 
 const StyledDiv = styled.div`
   border-style: outset;
@@ -22,6 +46,18 @@ export default function TeamMembers(props) {
   return (
     <div className="Team Members container">
       <StyledDiv>
+        <Rotate>
+          <span role="img" aria-label="Soccerball">
+            ⚽
+          </span>
+          <span role="img" aria-label="Programmer">
+            👨‍💻
+          </span>
+          <span role="img" aria-label="Laptop Computer">
+            💻
+          </span>
+        </Rotate>
+
         <p>Name: {details.name}</p>
         <p>Email: {details.email}</p>
         <p>Role: {details.role}</p>
