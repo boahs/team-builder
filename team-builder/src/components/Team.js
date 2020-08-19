2604219fc3c91784472bb377d89408624c60c584
boahs <39522778+boahs@users.noreply.github.com>
2020-08-19 04:15:36 -0400

    more style. more keyframe animations. wordbank array, function to grab random word.

--- team-builder/src/components/Team.js
+++ team-builder/src/components/Team.js
@@ -11,6 +11,55 @@ import styled, { keyframes } from "styled-components";
 // }
 // `;
 
+const stamp = keyframes`
+0%{
+    opacity: 0;
+  }
+  10%{
+    opacity:.50;
+    transform-origin: 50% 50%;
+    transform: rotate(-2deg) scale(5);
+    transition: all .3s cubic-bezier(0.6, 0.04, 0.98, 0.335);
+  }
+  100%{
+    opacity:1;
+    transform: rotate(-15deg) scale(1);
+  }
+`;
+
+const Stamp = styled.div`
+  height: 75px;
+  width: 100px;
+  left: 380px;
+  top: 110px;
+  border-radius: 5px;
+  position: relative;
+  background-color: #ef8080;
+  -webkit-animation-name: ${stamp};
+  animation-name: ${stamp};
+  animation-delay: 2.5s;
+  -webkit-animation-delay: 2.5s;
+  -webkit-animation-duration: 0.5s;
+  animation-duration: 0.5s;
+  opacity: 0;
+  -webkit-animation-fill-mode: forwards;
+  animation-fill-mode: forwards;
+`;
+
+const randomBank = [
+  "TEAM!",
+  "team!!!",
+  "group!",
+  "squad!!",
+  "bunch!",
+  "company!",
+  "party!",
+  "gang!",
+  "crew!!",
+];
+const randomMessage = randomBank[Math.floor(Math.random() * randomBank.length)];
+// console.log(randomMessage);
+
 const rotate = keyframes`
   from {
     transform: rotate(0deg);
@@ -22,10 +71,8 @@ const rotate = keyframes`
 `;
 const Rotate = styled.div`
   display: inline-block;
-  animation: ${rotate} 4s linear infinite;
-  padding: 2rem 1rem;
-  font-size: 1.2rem;
-  font-size: 1.2rem;
+  animation: ${rotate} 2s linear infinite;
+  font-size: 1.5rem;
 `;
 
 const StyledDiv = styled.div`
@@ -46,21 +93,34 @@ export default function TeamMembers(props) {
   return (
     <div className="Team Members container">
       <StyledDiv>
-        <Rotate>
-          <span role="img" aria-label="Soccerball">
-            ⚽
-          </span>
-          <span role="img" aria-label="Programmer">
-            👨‍💻
-          </span>
-          <span role="img" aria-label="Laptop Computer">
-            💻
-          </span>
-        </Rotate>
-
-        <p>Name: {details.name}</p>
-        <p>Email: {details.email}</p>
-        <p>Role: {details.role}</p>
+        <Stamp>welcome to the {randomMessage}</Stamp>
+        <p>
+          <Rotate>
+            {" "}
+            <span role="img" aria-label="Programmer">
+              👨‍💻
+            </span>
+          </Rotate>
+          Name: {details.name}
+        </p>
+        <p>
+          <Rotate>
+            {" "}
+            <span role="img" aria-label="Celebrating Face">
+              🥳
+            </span>
+          </Rotate>
+          Email: {details.email}
+        </p>
+        <p>
+          <Rotate>
+            {" "}
+            <span role="img" aria-label="Laptop computer ">
+              💻
+            </span>
+          </Rotate>
+          Role: {details.role}
+        </p>
       </StyledDiv>
     </div>
   );
